a7638f3ad9567ca76e394e9134fe48a6d0ff6d11
yd_zhang <petenlf1025@gmail.com>
2018-03-05 15:14:53 +0800

    完成606题

---
+++ Python3/606. Construct String from Binary Tree.py
@@ -0,0 +1,22 @@
+class Solution:
+    def tree2str(self, t):
+        """
+        :type t: TreeNode
+        :rtype: str
+        """
+        s = ''
+        def add2str(node):
+            if not node:
+                return '()'
+            elif not node.left and not node.right:
+                return '(' + str(node.val) + ')'
+            elif node.left:
+                return '(' + str(node.val) + add2str(node.left) + add2str(node.right) + ')'
+            else:
+                return '(' + str(node.val) + '(left)' + add2str(node.right) + ')'
+        
+        t = add2str(t)
+        t = t.split('()')
+        t = ''.join(t)
+        t = t.replace('(left)', '()')
+        return t[1: -1]
